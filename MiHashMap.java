fcb52712060862bc58cd7838b4964f3682f9e5a5
Alvaro <alvaroperbay97@gmail.com>
2016-03-07 13:14:47 +0100

    Creadas dos arrays en el constructor y el metodo put que permite añadir una clave y valor a las posiciones respectivas de los dos arrays

---
+++ MiHashMap.java
@@ -0,0 +1,56 @@
+
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    // Guarda las claves del hashmap como Strings
+    private String[] claves;
+    // Guarda los valores del hashmap como ints
+    private int[] valores; 
+
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        claves = new String[0];
+        valores = new int[0];
+    }
+
+    /**
+     * Asocia el valor especificado con la clave especificada. 
+     * Si la clave existía, entonces sobreescribe su valor 
+     * y devuelve el valor antiguo. Si no existía devuelve -1.
+     */
+    public int put(String clave, int valor)
+    {
+        boolean claveRep = false;
+        int numero = -1;
+        for (int count = 0; count < claves.length; count++) {
+            if (claves[count] == clave) {
+                numero = valores[count];
+                valores[count] = valor;
+                claveRep = true;
+            }
+        }
+        if(!claveRep){
+            String[] claves2 = claves;
+            claves = new String[claves2.length + 1];
+            int[] valores2 = valores;
+            valores = new int[valores2.length + 1];
+            
+            for (int count = 0; count < claves2.length; count++)
+            {
+                claves[count] = claves2[count];
+                valores[count] = valores2[count];
+            }
+            claves[claves2.length] = clave;
+            valores[valores2.length] = valor;
+        }
+        return numero;
+    }
+}
